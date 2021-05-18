267bbde6e9e71bec921120f1d49b028260b998ac
Fernando Jimenez Moreno <fjimenez@igalia.com>
2021-05-18 21:44:59 +0200

    Move session related enums to session

--- opentok/src/session.rs
+++ opentok/src/session.rs
@@ -1,14 +1,131 @@
 use crate::connection::Connection;
-use crate::enums::{IntoResult, OtcError, OtcResult, OtcSessionError};
+use crate::enums::{IntoResult, OtcError, OtcResult};
 
 use lazy_static::lazy_static;
 use std::collections::HashMap;
 use std::os::raw::{c_char, c_void};
 use std::rc::Rc;
 use std::sync::{Arc, Mutex};
+use thiserror::Error;
 
 lazy_static! {
     static ref SESSIONS: Arc<Mutex<HashMap<usize, Session>>> = Arc::new(Mutex::new(HashMap::new()));
+}
+
+/// Errors associated with an OpenTok session.
+#[derive(Clone, Copy, Debug, Eq, PartialEq, Hash, Error)]
+#[must_use]
+#[repr(u32)]
+pub enum OtcSessionError {
+    #[error("Authorization failure")]
+    AuthorizationFailure,
+    #[error("Block country")]
+    BlockedCountry,
+    #[error("Connection dropped")]
+    ConnectionDropped,
+    #[error("Connection failed")]
+    ConnectionFailed,
+    #[error("Connection limit exceeded")]
+    ConnectionLimitExceeded,
+    #[error("Connection refused")]
+    ConnectionRefused,
+    #[error("Connection timed out")]
+    ConnectionTimedOut,
+    #[error("Force unpublish or invalid stream")]
+    ForceUnpublishOrInvalidStream,
+    #[error("Illegal state")]
+    IllegalState,
+    #[error("Internal error")]
+    InternalError,
+    #[error("Invalid session")]
+    InvalidSession,
+    #[error("Invalid signal type")]
+    InvalidSignalType,
+    #[error("Not connected")]
+    NotConnected,
+    #[error("No messaging server")]
+    NoMessagingServer,
+    #[error("Null or invalid parameter")]
+    NullOrInvalidParameter,
+    #[error("Publisher not found")]
+    PublisherNotFound,
+    #[error("Signal data too long")]
+    SignalDataTooLong,
+    #[error("Singal type too long")]
+    SignalTypeTooLong,
+    #[error("State failed")]
+    StateFailed,
+    #[error("Subscriber not found")]
+    SubscriberNotFound,
+    #[error("Unexpected get session info response")]
+    UnexpectedGetSessionInfoResponse,
+    #[error("Unknown error")]
+    __Unknown,
+}
+
+impl From<ffi::otc_session_error_code> for OtcSessionError {
+    fn from(value: ffi::otc_session_error_code) -> OtcSessionError {
+        match value {
+            ffi::otc_session_error_code_OTC_SESSION_AUTHORIZATION_FAILURE => {
+                OtcSessionError::AuthorizationFailure
+            }
+            ffi::otc_session_error_code_OTC_SESSION_BLOCKED_COUNTRY => {
+                OtcSessionError::BlockedCountry
+            }
+            ffi::otc_session_error_code_OTC_SESSION_CONNECTION_DROPPED => {
+                OtcSessionError::ConnectionDropped
+            }
+            ffi::otc_session_error_code_OTC_SESSION_CONNECTION_FAILED => {
+                OtcSessionError::ConnectionFailed
+            }
+            ffi::otc_session_error_code_OTC_SESSION_CONNECTION_LIMIT_EXCEEDED => {
+                OtcSessionError::ConnectionLimitExceeded
+            }
+            ffi::otc_session_error_code_OTC_SESSION_CONNECTION_REFUSED => {
+                OtcSessionError::ConnectionRefused
+            }
+            ffi::otc_session_error_code_OTC_SESSION_CONNECTION_TIMED_OUT => {
+                OtcSessionError::ConnectionTimedOut
+            }
+            ffi::otc_session_error_code_OTC_SESSION_FORCE_UNPUBLISH_OR_INVALID_STREAM => {
+                OtcSessionError::ForceUnpublishOrInvalidStream
+            }
+            ffi::otc_session_error_code_OTC_SESSION_ILLEGAL_STATE => OtcSessionError::IllegalState,
+            ffi::otc_session_error_code_OTC_SESSION_INTERNAL_ERROR => {
+                OtcSessionError::InternalError
+            }
+            ffi::otc_session_error_code_OTC_SESSION_INVALID_SESSION => {
+                OtcSessionError::InvalidSession
+            }
+            ffi::otc_session_error_code_OTC_SESSION_INVALID_SIGNAL_TYPE => {
+                OtcSessionError::InvalidSignalType
+            }
+            ffi::otc_session_error_code_OTC_SESSION_NOT_CONNECTED => OtcSessionError::NotConnected,
+            ffi::otc_session_error_code_OTC_SESSION_NO_MESSAGING_SERVER => {
+                OtcSessionError::NoMessagingServer
+            }
+            ffi::otc_session_error_code_OTC_SESSION_NULL_OR_INVALID_PARAMETER => {
+                OtcSessionError::NullOrInvalidParameter
+            }
+            ffi::otc_session_error_code_OTC_SESSION_PUBLISHER_NOT_FOUND => {
+                OtcSessionError::PublisherNotFound
+            }
+            ffi::otc_session_error_code_OTC_SESSION_SIGNAL_DATA_TOO_LONG => {
+                OtcSessionError::SignalDataTooLong
+            }
+            ffi::otc_session_error_code_OTC_SESSION_SIGNAL_TYPE_TOO_LONG => {
+                OtcSessionError::SignalTypeTooLong
+            }
+            ffi::otc_session_error_code_OTC_SESSION_STATE_FAILED => OtcSessionError::StateFailed,
+            ffi::otc_session_error_code_OTC_SESSION_SUBSCRIBER_NOT_FOUND => {
+                OtcSessionError::SubscriberNotFound
+            }
+            ffi::otc_session_error_code_OTC_SESSION_UNEXPECTED_GET_SESSION_INFO_REPONSE => {
+                OtcSessionError::UnexpectedGetSessionInfoResponse
+            }
+            _ => OtcSessionError::__Unknown,
+        }
+    }
 }
 
 ffi_callback!(on_connected);
@@ -96,8 +213,7 @@ pub struct SessionCallbacks {
     on_stream_video_dimensions_changed: Option<Box<dyn Fn(*const ffi::otc_stream, i32, i32)>>,
     on_stream_video_type_changed:
         Option<Box<dyn Fn(*const ffi::otc_stream, ffi::otc_stream_video_type)>>,
-    on_signal_received:
-        Option<Box<dyn Fn(*const c_char, *const c_char, Connection)>>,
+    on_signal_received: Option<Box<dyn Fn(*const c_char, *const c_char, Connection)>>,
     on_archive_started: Option<Box<dyn Fn(*const c_char, *const c_char)>>,
     on_archive_stopped: Option<Box<dyn Fn(*const c_char)>>,
     on_error: Option<Box<dyn Fn(*const c_char, OtcSessionError)>>,
@@ -129,16 +245,8 @@ impl SessionCallbacks {
     callback!(on_reconnection_started);
     callback!(on_reconnected);
     callback!(on_disconnected);
-    callback!(
-        on_connection_created,
-        connection,
-        Connection
-    );
-    callback!(
-        on_connection_dropped,
-        connection,
-        Connection
-    );
+    callback!(on_connection_created, connection, Connection);
+    callback!(on_connection_dropped, connection, Connection);
     callback!(on_stream_received, stream, *const ffi::otc_stream);
     callback!(on_stream_dropped, stream, *const ffi::otc_stream);
     callback!(
@@ -211,8 +319,7 @@ pub struct SessionCallbacksBuilder {
     on_stream_video_dimensions_changed: Option<Box<dyn Fn(*const ffi::otc_stream, i32, i32)>>,
     on_stream_video_type_changed:
         Option<Box<dyn Fn(*const ffi::otc_stream, ffi::otc_stream_video_type)>>,
-    on_signal_received:
-        Option<Box<dyn Fn(*const c_char, *const c_char, Connection)>>,
+    on_signal_received: Option<Box<dyn Fn(*const c_char, *const c_char, Connection)>>,
     on_archive_started: Option<Box<dyn Fn(*const c_char, *const c_char)>>,
     on_archive_stopped: Option<Box<dyn Fn(*const c_char)>>,
     on_error: Option<Box<dyn Fn(*const c_char, OtcSessionError)>>,
@@ -248,12 +355,7 @@ impl SessionCallbacksBuilder {
         *const ffi::otc_stream,
         ffi::otc_stream_video_type
     );
-    callback_setter!(
-        on_signal_received,
-        *const c_char,
-        *const c_char,
-        Connection
-    );
+    callback_setter!(on_signal_received, *const c_char, *const c_char, Connection);
     callback_setter!(on_archive_started, *const c_char, *const c_char);
     callback_setter!(on_archive_stopped, *const c_char);
     callback_setter!(on_error, *const c_char, OtcSessionError);
@@ -368,16 +470,8 @@ impl Session {
     callback_call!(on_reconnection_started);
     callback_call!(on_reconnected);
     callback_call!(on_disconnected);
-    callback_call!(
-        on_connection_created,
-        connection,
-        Connection
-    );
-    callback_call!(
-        on_connection_dropped,
-        connection,
-        Connection
-    );
+    callback_call!(on_connection_created, connection, Connection);
+    callback_call!(on_connection_dropped, connection, Connection);
     callback_call!(on_stream_received, stream, *const ffi::otc_stream);
     callback_call!(on_stream_dropped, stream, *const ffi::otc_stream);
     callback_call!(
