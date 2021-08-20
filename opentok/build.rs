595fdc56699b44559ea6c9d8e542a2160fdec20d
Philippe Normand <philn@igalia.com>
2021-08-20 09:00:15 +0100

    CI: Add basic workflow

---
+++ opentok/build.rs
@@ -0,0 +1,8 @@
+use std::env;
+
+fn main() {
+    if let Ok(sdk_dir) = env::var("OPENTOK_PATH") {
+        let root_dir = env::var("CARGO_MANIFEST_DIR").unwrap();
+        println!("cargo:rustc-link-search=native={}/../{}/lib", root_dir, sdk_dir);
+    }
+}
