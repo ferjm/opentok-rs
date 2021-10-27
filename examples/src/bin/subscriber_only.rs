dd361453ecd7f58e333e2ed760048b083c0fe853
Fernando Jimenez Moreno <fjimenez@igalia.com>
2021-10-27 18:05:56 +0200

    examples: add subscriber only example

---
+++ examples/src/bin/subscriber_only.rs
@@ -0,0 +1,23 @@
+extern crate opentok;
+
+use opentok::log::{self, LogLevel};
+use opentok_utils::subscriber::Subscriber;
+
+#[path = "../cli.rs"]
+mod cli;
+
+#[async_std::main]
+async fn main() -> anyhow::Result<()> {
+    let (credentials, duration) = cli::parse_cli().await?;
+
+    opentok::init()?;
+
+    log::enable_log(LogLevel::Error);
+    log::logger_callback(Box::new(|msg| {
+        println!("{:?}", msg);
+    }));
+
+    Subscriber::new(credentials, duration).run()?;
+
+    Ok(opentok::deinit()?)
+}
