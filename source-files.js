1429855ebf8a6556d526b91846746f4f34159772
ferjm <ferjm@users.noreply.github.com>
2021-11-12 09:34:10 +0000

    deploy: fc7f963e413e4c9bc9d6616099e20a10c1d8a3fa

--- source-files.js
+++ source-files.js
@@ -2,7 +2,6 @@ var N = null;var sourcesIndex = {};
 sourcesIndex["basic_video_chat"] = {"name":"","files":["basic_video_chat.rs","cli.rs"]};
 sourcesIndex["custom_video_capturer"] = {"name":"","files":["cli.rs","custom_video_capturer.rs"]};
 sourcesIndex["opentok"] = {"name":"","files":["audio_device.rs","connection.rs","enums.rs","lib.rs","log.rs","macros.rs","publisher.rs","session.rs","stream.rs","subscriber.rs","video_capturer.rs","video_frame.rs"]};
-sourcesIndex["opentok_rs_sys"] = {"name":"","files":["lib.rs"]};
 sourcesIndex["opentok_utils"] = {"name":"","files":["capturer.rs","common.rs","lib.rs","publisher.rs","renderer.rs","subscriber.rs"]};
 sourcesIndex["publisher_only"] = {"name":"","files":["cli.rs","publisher_only.rs"]};
 sourcesIndex["subscriber_only"] = {"name":"","files":["cli.rs","subscriber_only.rs"]};
